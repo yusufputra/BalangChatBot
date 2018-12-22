e470e41c63f9896a32bce694d961ef71dfba5d30
Glitch (hello-express) <none>
2018-12-22 14:29:22 +0000

    😾🎨 Checkpoint ./server.js:2612722/2419

--- server.js
+++ server.js
@@ -1,22 +1,64 @@
-// server.js
-// where your node app starts
+// // server.js
+// // where your node app starts
 
-// init project
+// // init project
+// const express = require('express');
+// const app = express();
+
+// // we've started you off with Express, 
+// // but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// // http://expressjs.com/en/starter/static-files.html
+// app.use(express.static('public'));
+
+// // http://expressjs.com/en/starter/basic-routing.html
+// app.get('/', function(request, response) {
+//   response.sendFile(__dirname + '/views/index.html');
+// });
+
+// // listen for requests :)
+// const listener = app.listen(process.env.PORT, function() {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
+
+const line = require('@line/bot-sdk');
 const express = require('express');
+const axios = require('axios');
+ 
+const config = {
+  channelAccessToken: "9q1vjHNqSV1wTBV+tiFMFeee1vhzpngxISCHGxvp0dNzmuIXFREOmoh4+ovBP85R1KHHpfK0FyBbtRBkJLmHhv7I4pvzDtdtkAYNa8FJk7bGEcvMfGoVtwcYKezrUJvVdOYuWmdnpSxZ+sg8cbcqhwdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "56e65e8cba9dca267c064ff0dff3ef6e",
+};
+ 
+// create LINE SDK client
+const client = new line.Client(config);
 const app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+ 
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((e)=>{
+      console.log(e);
+    });
+ 
 });
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+ 
+function handleEvent(event) {
+ 
+    if(event.message.text == "hai"){
+      const echo = { type: 'text', text: "Halo juga :)·" };
+      return client.replyMessage(event.replyToken, echo);
+    }
+ 
+    const echo = { type: 'text', text: "Saya tidak mengerti, saya simpan dulu" };
+    return client.replyMessage(event.replyToken, echo);
+}
+ 
+// listen on port
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
