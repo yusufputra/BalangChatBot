660b9776befc94fa62f53d39239c99d6d2392ff5
Glitch (butter-mail) <none>
2018-12-24 10:24:09 +0000

    🍤💉 Checkpoint ./api/balang.js:2612722/642

--- api/balang.js
+++ api/balang.js
@@ -15,4 +15,22 @@ router.get('/barang',(req,res,next)=>{
     })
 })
 
+router.post('/postThread',(req,res,next)=>{
+    console.log(req.user);
+    let query="insert into thread(idUser,title,post,username)" +
+    "values("+req.user._id+",'"+req.body.title+"','"+req.body.post+"','"+req.user.username+"')"
+    console.log(query);
+    knex.schema.raw(query).then(ress=>{
+        res.json('done');
+    }).catch(err=>{
+        res.status(404);
+        res.json(err);
+    })
+})
+
+router.post('/postBarang',(req,res,next)=>{
+  console.log('post Barang');
+  let query = "insert into daftarBarang (nama, pemilik, lokasiBarang) values ('req.nama','req.pemilik','req.lokasiBarang')";
+})
+
 module.exports = router;
