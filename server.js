fd44f375a29925d6b28029535028c0e5e458149f
Glitch (hello-express) <none>
2018-12-24 12:53:14 +0000

    💇👓 Checkpoint ./server.js:2612722/5015 ./db/connectionAzure.js:2612722/268 ./api/balang.js:2612722/809

--- server.js
+++ server.js
@@ -1,22 +1,135 @@
-// server.js
-// where your node app starts
-
-// init project
+const line = require('@line/bot-sdk');
 const express = require('express');
+const axios = require('axios');
+const options = require('./db/connectionAzure');
+var knex = require('knex')(options);
+const fetch = require('node-fetch');
+const {Line} = require('messaging-api-line');
+
+
+const volleyball = require('volleyball');
+require('dotenv').config();
+const api = require('./api/balang');
+
+ 
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+ 
+// create LINE SDK client
+const client = new line.Client(config);
 const app = express();
+const parser = express.json();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+app.use(volleyball);
+app.use('/api',api);
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((e)=>{
+      console.log(e);
+    });
+ 
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.get('/',(req,res)=>{
+    res.json("halooo")
+})
+ 
+function handleEvent(event) {
+    console.log('event')
+    console.log(event.message.text)
+    if(event.message.text == "hai"){
+      const echo = { type: 'text', text: "Halo juga :)·" };
+      return client.replyMessage(event.replyToken, [
+        Line.createText('Hello'),
+        Line.createText('End'),
+      ]);
+    }else if(event.message.text == "barang"){
+      let echo = "Daftar Barang\n==============";
+      axios.get('https://butter-mail.glitch.me/api/barang').then(function(res){
+        res.data.map(function(result){
+          echo += "\nNama Barang : " + result.nama + "\n" +"Pemilik Barang : " + result.pemilik + "\n" +"Lokasi Barang : " + result.lokasiBarang+  "\n--------------";
+        }).join('');
+        return client.replyMessage(event.replyToken,[
+            Line.createText(echo),
+          ]);
+      }).catch(function(err){
+        console.log("error");
+        console.log(err);
+      })
+    }else if(event.message.text == "sayang kamu"){
+      const echo = { type: 'text', text: "sayang kamu juga :)·" };
+      return client.replyMessage(event.replyToken, echo);
+    }else if(event.message.text.startsWith("ditemukan")){
+      console.log(event.message.text);
+      let statement = event.message.text.split(" ");
+      let data = statement[1].split("/");
+      const body = {
+        nama : data[0],
+        pemilik : data[1],
+        lokasiBarang : data[2]
+      }
+      // console.log(body);
+      // axios.post('https://butter-mail.glitch.me/api/postBarang',parser, {
+      //   body : {
+      //     nama : data[0],
+      //     pemilik : data[1],
+      //     lokasiBarang : data[2]
+      //   }
+      // })
+      // .then(function (response) {
+      //   // console.log(response);
+      //   const echo = { type: 'text', text: "saved" };
+      //   return client.replyMessage(event.replyToken, echo);
+      // })
+      // .catch(function (error) {
+      //   // console.log(error);
+      //   const echo = { type: 'text', text: "error" };
+      //   return client.replyMessage(event.replyToken, echo);
+      // });
+      
+      fetch ('https://butter-mail.glitch.me/api/postBarang',parser,{
+      method: 'POST',
+      headers:{
+        'content-type':'application/json',
+      },
+      body:JSON.stringify(body),
+    }).then(response=>{
+      if(response.ok){
+        const echo = { type: 'text', text: "saved" };
+        return client.replyMessage(event.replyToken, echo);
+        
+      }
+      return response.json().then(error=>{
+        console.log("error1");
+        console.log(error.message);
+        const echo = { type: 'text', text: error.message };
+        return client.replyMessage(event.replyToken, echo);
+      });
+    }).catch(error=>{
+      // console.log('fetch error'+error)
+      // this.setState({ errorMessage: error.message });
+      // this.setState({login:false})
+        console.log("error2");
+        console.log(error.message);
+        const echo = { type: 'text', text: error.message};
+        return client.replyMessage(event.replyToken, echo);
+    });
+      
+    }else{
+      const echo = { type: 'text', text: "Saya tidak mengerti, saya simpan dulu" };
+      return client.replyMessage(event.replyToken, echo);
+    }
+}
+ 
+// listen on port
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
